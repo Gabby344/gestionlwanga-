815a0f271a6f597fc2f423f4e1d0bf4f79d75ebe
Gabby Umba <159041433+Gabby344@users.noreply.github.com>
2025-10-07 21:32:14 +0200

    Create roles.js

---
+++ roles.js
@@ -0,0 +1,12 @@
+// roles.js
+export const roleRedirects = {
+  admin: "accueil-admin.html",
+  prefet: "accueil-admin.html",
+  directeur_etudes: "dashboard.html",
+  directeur_discipline: "dashboard.html",
+  secretaire: "dashboard.html",
+  econome: "finance.html",
+  enseignant: "accueil-enseignant.html",
+  eleve: "accueil-utilisateur.html",
+  parent: "accueil-utilisateur.html",
+};
